69eedeca20a3ae5ea0091553bdf8329a96bb67ea
Eito Mizuno <185331637+mizuei2525810@users.noreply.github.com>
2026-06-09 14:53:13 +0900

    add src directory

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Study Record System");
+        System.out.println("1. 記録する");
+        System.out.println("2. 一覧表示");
+        System.out.println("3. 終了");
+    }
+}
